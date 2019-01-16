6b22f672af48f45b21113765851d21831f2d5e02
karolborecki <karol.borecki@onet.eu>
2019-01-16 21:35:22 +0100

    Boundries Generator Script has been added

---
+++ SnakeRun/Assets/Scripts/BoundriesGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundriesGenerator : MonoBehaviour {
+
+    public Transform boundryPrefab;
+    public Transform relativityObject;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
