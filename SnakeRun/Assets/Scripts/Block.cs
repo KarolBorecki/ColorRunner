cc98ca1f374a2e3a2b7e60b8c30e7046d7c2ce53
karolborecki <karol.borecki@onet.eu>
2019-01-16 21:30:10 +0100

    Block responsivity to screen has been added

---
+++ SnakeRun/Assets/Scripts/Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    void Start()
+    {
+        SetSize();
+    }
+
+    void SetSize()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2;
+
+        transform.localScale = new Vector3(transform.localScale.x, worldScreenHeight / sr.sprite.bounds.size.y, 1);
+    }
+}
