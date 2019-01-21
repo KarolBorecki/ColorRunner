b0b7b3d9fbbc8c7915cf8a9a90af66e2c9009250
karolborecki <karol.borecki@onet.eu>
2019-01-21 23:11:14 +0100

    Shooting has been added

--- SnakeRun/Assets/Scripts/Ball.cs
+++ SnakeRun/Assets/Scripts/Ball.cs
@@ -6,6 +6,13 @@ public class Ball : MonoBehaviour {
 
     public int lives = 1;
 
+    public float shootDelay = .1f;
+    private float actuallTime = 0f;
+
+    public float shootForce = 1000f;
+    public Rigidbody2D shoot;
+    public Transform shootTransform;
+
     void Start () {
 		
 	}
@@ -14,7 +21,20 @@ public class Ball : MonoBehaviour {
         Vector3 mouse = Input.mousePosition;
         mouse = Camera.main.ScreenToWorldPoint(mouse);
 
-        transform.position = new Vector3(mouse.x, mouse.y, 0.0f);
+        transform.position = new Vector3(mouse.x, mouse.y, transform.position.y);
+
+        if (Input.GetButton("Fire1")) Shoot();
+    }
+
+    void Shoot()
+    {
+        actuallTime += Time.deltaTime;
+        if (actuallTime >= shootDelay)
+        {
+            Rigidbody2D shootball = Instantiate(shoot, shootTransform.position, transform.rotation) as Rigidbody2D;
+            shootball.AddForce(Vector2.right * shootForce);
+            actuallTime = 0f;
+        }
     }
 
     public void GetDamage(int amount){
